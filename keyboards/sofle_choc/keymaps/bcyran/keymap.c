8c8061029cff5cdc82628a544b9b7584fb44aa68
Bazyli Cyran <bazyli.cyran@gmail.com>
2023-12-13 20:33:51 +0100

    feat: remake mod layers

--- keyboards/sofle_choc/keymaps/bcyran/keymap.c
+++ keyboards/sofle_choc/keymaps/bcyran/keymap.c
@@ -21,27 +21,24 @@ enum layer_names {
     _FN,
 };
 
-#define RGUI_SP RGUI_T(KC_SPC)
-#define LGUI_SP LGUI_T(KC_SPC)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QW] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_LCTL, KC_LGUI, MO(_SM), KC_LALT,  KC_ENT, KC_SPC,  KC_SPC,  KC_RALT, MO(_FN), KC_RCTL
+                          KC_LGUI, KC_LCTL, MO(_SM), KC_LALT,  KC_ENT, KC_ENT,  KC_SPC,  KC_RALT, MO(_FN), KC_RCTL
     ),
     [_SM] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                     KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_F12,
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-        _______, KC_PEQL, KC_PMNS, KC_PPLS, KC_LCBR, KC_RCBR, _______, _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
-                          _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______
+        _______, _______, _______, _______, _______, _______,                   _______, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, _______,
+        _______, _______, _______, KC_LCBR, KC_RCBR, KC_PIPE,                   _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_DEL,
+        _______, _______, _______, KC_LPRN, KC_RPRN, KC_MINS,                   _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
+        _______, _______, _______, KC_LBRC, KC_RBRC, KC_UNDS, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, _______,
+                          _______, _______, _______, _______, _______, _______, _______, KC_P0,   _______, KC_PDOT
     ),
     [_FN] = LAYOUT(
-        QK_BOOT, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                   _______, KC_PGDN, KC_PGUP, _______, _______, _______,
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        QK_RBT,  _______, _______, _______, _______, _______,                   _______, KC_PGDN, KC_PGUP, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
@@ -51,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     //     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    //                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    //                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     // ),
 };
 
