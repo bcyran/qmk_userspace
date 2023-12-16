ee979cf2841b182888da41457b3b2688a35efd1e
Bazyli Cyran <bazyli.cyran@gmail.com>
2023-12-16 16:56:15 +0100

    Adjust the keymap

--- keyboards/sofle_choc/keymaps/bcyran/keymap.c
+++ keyboards/sofle_choc/keymaps/bcyran/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_SM] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                   _______, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, _______,
-        _______, _______, _______, KC_LCBR, KC_RCBR, KC_PIPE,                   _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_DEL,
-        _______, _______, _______, KC_LPRN, KC_RPRN, KC_MINS,                   _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
-        _______, _______, _______, KC_LBRC, KC_RBRC, KC_UNDS, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, _______,
+        _______, KC_ASTR, KC_MINS, KC_LCBR, KC_RCBR, KC_PIPE,                   _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_DEL,
+        KC_CAPS, KC_AMPR, KC_UNDS, KC_LPRN, KC_RPRN, KC_EQL,                    _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
+        _______, KC_EXLM, KC_HASH, KC_LBRC, KC_RBRC, KC_PLUS, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, _______,
                           _______, _______, _______, _______, _______, _______, _______, KC_P0,   _______, KC_PDOT
     ),
     [_FN] = LAYOUT(
         QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        QK_RBT,  _______, _______, _______, _______, _______,                   _______, KC_PGDN, KC_PGUP, _______, _______, _______,
+        QK_RBT,  _______, _______, _______, _______, _______,                   _______, KC_PGDN, KC_PGUP, _______, _______, KC_F12,
         _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
